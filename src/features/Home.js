b76d0012e7dfc2408a7d9bf75ac4035e57c7e45c
Zoeytothemoon <worawichyimsabai@gmail.com>
2025-10-11 12:45:00 +0700

    assignment 07

--- src/features/Home.js
+++ src/features/Home.js
@@ -15,14 +15,18 @@ export default function Home() {
   }
 
   return (
-    <div>
+    <>
       <h1>New Products</h1>
-      <ul className="Home__products">
-        {products.map((product) => (
-          <Product key={product.id} item={product} />
-        ))}
-      </ul>
+      {products.length > 0 ? (
+        <ul className="Home__products">
+          {products.map((product) => (
+            <Product key={product.id} item={product} />
+          ))}
+        </ul>
+      ) : (
+        <div>Loading products....</div>
+      )}
       <AddForm addProduct={addProduct} />
-    </div>
+    </>
   );
 }
